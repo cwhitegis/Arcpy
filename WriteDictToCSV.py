3426d9ba3b0fefe22bd0c242c3c04d6be051500f
cwhitegis <cwhitegis@gmail.com>
2024-07-31 19:52:28 -1000

    update

--- WriteDictToCSV.py
+++ WriteDictToCSV.py
@@ -1,18 +1,20 @@
 import arcpy
 import csv
-
+#Run script outside of ArcGIS Pro
 aprx = arcpy.mp.ArcGISProject(r"C:\ProjectLocation\Test\Test.aprx")
 mp = aprx.listMaps('Map_Name')[0]
 lyr = mp.listLayers('Layer_Name')[0]
 
 ownerDict = {}
-fields = ['field1','field2', 'field3']
+fields = ['owner','parcelID', 'acres']
 
 with arcpy.da.SearchCursor(lyr,fields) as cursor:
     for row in cursor:
+        #create new owner key and add values only if it doesnt exist
         if row[0] not in ownerDict.keys() and row[1] is not None:
             ownerDict[row[0]] = [[row[1]],[row[2]]]
         else:
+            #if owner does exist and parcel doesnt exist then append parcel and area
             if row[1] is not None:
                 if row[1] not in ownerDict[row[0]][0]:
                         ownerDict[row[0]][0].append(row[1])
@@ -22,7 +24,7 @@ file = open(r"C:\Users\Documents\CSV_Location.csv", 'w', newline='')
 
 with file as csvFile:
     writer = csv.writer(csvFile)
-    writer.writerow(['field1','field2', 'field3'])
+    writer.writerow(['owner','parcelID', 'acres'])
     for k, v in ownerDict.items():
         #Write CSV with field 1,the length of field 2, and sum field 3 rounded to 2 decimals
         writer.writerow([k, len(v[0]),round(sum(v[1]),2)])
